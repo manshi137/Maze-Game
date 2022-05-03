822da107ce471162fd4713947fb98b86afbf7e8f
manshi137 <86864034+manshi137@users.noreply.github.com>
2022-05-04 01:14:59 +0530

    all png files used in game till now

---
+++ 30_scrolling/pics/5-removebg-preview.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.4" name="5-removebg-preview" tilewidth="32" tileheight="32" tilecount="9" columns="3">
+ <image source="../../../Downloads/5-removebg-preview.png" width="96" height="96"/>
+</tileset>
